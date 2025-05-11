2b057920691447b387010b7eaaee16f9931ff5cd
goktas-batuhan <batuhan.goktas.dev@gmail.com>
2025-05-11 13:02:46 +0300

    feat :: integrated react-hook-form with reactstrap inputs and added validation for email and password

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,36 +1,91 @@
 import { Button, Form, FormText, Input, Label, FormGroup } from "reactstrap"
+import { useForm } from "react-hook-form"
 import "./LoginPage.css"
 function App() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+        reset,
+    } = useForm({
+        mode: 'onChange',
+        defaultValues:{
+            email:"",
+            password:"",
+        }
+    });
+
+
+    const onSubmit = (data) => {
+        console.log(data)
+
+    }
+
+    console.log("Form hataları:", errors);
+
+
+
+
+    const { ref, ...registerEmail } = register("email", {
+        required: "Email is required",
+        pattern: {
+            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: "Please enter a valid email",
+        },
+    });
+
+    const { ref: passwordRef, ...registerPassword } = register("password", {
+        required: "Password is required",
+        validate: validatePassword
+    });
+
+    
+    function validatePassword(password) {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return regex.test(password) || "Şifre en az 8 karakter, büyük harf, küçük harf, sayı ve özel karakter içermeli.";
+    }
+
+
+
 
 
     return (
         <div className="all-page">
             <div className="blur">
-                <Form className="Inner">
+                <Form className="Inner" onSubmit={handleSubmit(onSubmit)}>
                     <FormGroup>
                         <h1 className="title">Login</h1>
                     </FormGroup>
                     <FormGroup>
                         <Label for="email">
-                            
+
                         </Label>
                         <Input
                             id="email"
-                            name="email"
                             placeholder="Type your email here..."
                             type="email"
+                            innerRef={ref}
+                            {...registerEmail}
+
                         />
+                        {errors.email && <p>{errors.email.message}</p>}
+
                     </FormGroup>
                     <FormGroup>
                         <Label for="password">
-                            
+
                         </Label>
                         <Input
                             id="password"
-                            name="password"
                             placeholder="type your password here..."
                             type="password"
+                            innerRef={passwordRef}
+                            {...registerPassword}
+
+
                         />
+                        {errors.password && <p>{errors.password.message}</p>}
+
                     </FormGroup>
                     <FormGroup check>
                         <Input type="checkbox" />
@@ -39,7 +94,7 @@ function App() {
                             Remember me
                         </Label>
                     </FormGroup>
-                    <Button className="login-button">
+                    <Button type="submit" className="login-button">
                         Login
                     </Button>
                 </Form>
